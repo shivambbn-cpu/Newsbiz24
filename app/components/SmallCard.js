f9a7f69632ee80b502359bcfd98caa3b5b413fb8
shivambbn-cpu <shivam.bbn@gmail.com>
2025-12-31 22:01:03 +0530

    Update SmallCard.js

--- app/components/SmallCard.js
+++ app/components/SmallCard.js
@@ -1,45 +1,33 @@
 "use client";
 
 export default function SmallPostCard({ post, onSelectPost }) {
-  return (
-    <div
-      className="small-card post-card"
-      onClick={() => onSelectPost(post)}
-    >
-      {/* IMAGE */}
-      <img
-        src={post.image}
-        alt={post.title}
-        className="small-img"
-      />
+return (
+<div
+className="small-card post-card"
+onClick={() => onSelectPost(post)}
+>
+{/* IMAGE */}
+<img  
+src={post.image}  
+alt={post.title}  
+className="small-img"  
+/>
 
-      {/* TEXT BLOCK */}
-      <div>
-        <h4>{post.title}</h4>
+{/* TEXT BLOCK */}  
+  <div>  
+    <h4>{post.title}</h4>  
 
-        {/* CONTENT with <br> and <strong> support */}
-        <div
-          className="small-content"
-          dangerouslySetInnerHTML={{
-            __html: ((post.content || "").substring(0, 100) + "...").replace(
-              /\n/g,
-              "<br>"
-            ),
-          }}
-        ></div>
+    {/* CONTENT */}  
+    <p>  
+      {(post.content || "").substring(0, 100)}...  
+    </p>  
 
-        {/* DATE */}
-        <small>
-          <strong>
-            Posted on :{" "}
-            {new Date(post.date).toLocaleDateString("en-IN", {
-              day: "2-digit",
-              month: "long",
-              year: "numeric",
-            })}
-          </strong>
-        </small>
-      </div>
-    </div>
-  );
+    {/* DATE */}  
+    <small>  
+      {new Date(post.date).toLocaleDateString("en-IN")}  
+    </small>  
+  </div>  
+</div>
+
+);
 }
