c74532e8a74364b0d1ac92a06f882c3199abfa37
shivambbn-cpu <shivam.bbn@gmail.com>
2025-12-31 11:24:20 +0530

    Create SideMenu.js

---
+++ app/components/SideMenu.js
@@ -0,0 +1,18 @@
+"use client";
+
+export default function SideMenu() {
+  return (
+    <div id="side-menu" className="side-menu">
+      <ul>
+        <li className="active">Religious</li>
+        <li>Weather</li>
+        <li>Trending</li>
+        <li>Astro</li>
+        <li>Business</li>
+        <li>Health</li>
+        <li>Lifestyles</li>
+        <li>News</li>
+      </ul>
+    </div>
+  );
+}
