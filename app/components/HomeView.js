aaf17f0f55dd6f3f14e3d127c390c0de7afc4989
shivambbn-cpu <shivam.bbn@gmail.com>
2025-12-31 11:32:17 +0530

    Create HomeView.js

---
+++ app/components/HomeView.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function HomeView() {
+  return (
+    <div id="home-view">
+      <div id="big-card-container"></div>
+      <div id="small-cards-container" className="posts-list-container"></div>
+    </div>
+  );
+}
