a7451c9d113b1114c8116b82b12594cda578ad4e
shivambbn-cpu <shivam.bbn@gmail.com>
2025-12-31 11:30:18 +0530

    Create Footer.js

---
+++ app/components/Footer.js
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-buttons">
+        <button onClick={() => openModal("about")}>About Us</button>
+        <button onClick={() => openModal("contact")}>Contact Us</button>
+        <button onClick={() => openModal("privacy")}>Privacy Policy</button>
+        <button onClick={() => openModal("terms")}>Terms & Conditions</button>
+      </div>
+
+      <div className="footer-copy">
+        © 2025 newsbiz24.in All rights reserved.
+      </div>
+    </footer>
+  );
+    }
