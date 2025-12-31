c2fc53caea6ed7c725c0936a834cead22291d3f5
shivambbn-cpu <shivam.bbn@gmail.com>
2025-12-31 23:36:06 +0530

    Update globalSearch.js

--- app/lib/globalSearch.js
+++ app/lib/globalSearch.js
@@ -1,1 +1,29 @@
+// app/lib/globalSearch.js
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { CATEGORIES } from "./categories";
 
+/**
+ * Fetch all posts from all categories
+ */
+export async function getAllPostsForSearch() {
+  let allPosts = [];
+
+  for (const category of CATEGORIES) {
+    const snapshot = await getDocs(collection(db, category));
+
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      allPosts.push({
+        id: doc.id,
+        category: category,
+        title: data.title || "",
+        slug: data.slug || "",
+        image: data.image || "",
+        date: data.date || null,
+      });
+    });
+  }
+
+  return allPosts;
+}
