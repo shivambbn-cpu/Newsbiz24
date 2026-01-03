89f4b22343e0addf9eab7920d151be25e84f7031
shivambbn-cpu <shivam.bbn@gmail.com>
2026-01-03 11:59:16 +0530

    Update page.js

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -1,7 +1,7 @@
-import Header from "@/components/Header";
-import SideMenu from "@/components/SideMenu";
-import HomeView from "@/components/HomeView";
-import Footer from "@/components/Footer";
+import Header from "@/app/components/Header",
+import SideMenu from "@/app/components/SideMenu",
+import HomeView from "@/app/components/HomeView";
+import Footer from "@/app/components/Footer";
 
 import { db } from "@/lib/firebaseAdmin";
 import {
