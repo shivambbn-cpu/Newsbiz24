51d8e429f8a7b980719c79fdda0a543cc57d38c0
shivambbn-cpu <shivam.bbn@gmail.com>
2026-01-12 17:52:50 +0530

    Update RelatedPosts.js

--- app/components/RelatedPosts.js
+++ app/components/RelatedPosts.js
@@ -1,12 +1,1 @@
-"use client";
 
-export default function RelatedPosts({ posts }) {
-  return (
-    <div style={{ padding: 20, background: "#e8f3ff" }}>
-      <h3>Related Posts TEST</h3>
-      {posts?.map((p) => (
-        <div key={p.slug}>{p.title}</div>
-      ))}
-    </div>
-  );
-}
