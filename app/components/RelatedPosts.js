fbb2585002144a7ad6e9c75fb422556e04dfa61f
shivambbn-cpu <shivam.bbn@gmail.com>
2026-01-12 17:53:45 +0530

    Update RelatedPosts.js

--- app/components/RelatedPosts.js
+++ app/components/RelatedPosts.js
@@ -1,1 +1,11 @@
+"use client";
 
+export default function RelatedPosts({ posts }) {
+  return (
+    <div style={{ padding: 20, background: "#e8f3ff" }}>
+      <h3>Related Posts TEST</h3>
+
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </div>
+  );
+}
