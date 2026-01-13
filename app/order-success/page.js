76c5732745124e26eac20ff30f2ee91cdfb22ba2
shivambbn-cpu <shivam.bbn@gmail.com>
2026-01-13 10:54:38 +0530

    Create page.js

---
+++ app/order-success/page.js
@@ -0,0 +1,8 @@
+export default function OrderSuccess() {
+  return (
+    <div style={{ textAlign: "center", padding: 50 }}>
+      <h1>✅ Order Placed Successfully</h1>
+      <p>We will contact you soon.</p>
+    </div>
+  );
+}
