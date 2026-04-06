96e8f3fd765ab06575bfd69914526362dab172b0
shivambbn-cpu <shivam.bbn@gmail.com>
2026-04-06 18:38:49 +0530

    Implement dynamic metadata generation for posts
    Add generateMetadata function to create dynamic page metadata

--- app/post/[slug]/layout.js
+++ app/post/[slug]/layout.js
@@ -1,1 +1,19 @@
+export async function generateMetadata({ params }) {
+  const slug = params.slug;
 
+  const title = slug.replace(/-/g, " ");
+
+  return {
+    title: title + " | Newsbiz24",
+
+    // 👇 Global description use कर रहे हैं
+    description:
+      "Newsbiz24.in पर पाएं धार्मिक, ट्रेंडिंग, एस्ट्रो, व्यापार, स्वास्थ्य, जीवनशैली और ताज़ा खबरों की कवरेज।",
+
+    keywords: `${title}, gharelu nuskhe, hindi health tips`,
+  };
+}
+
+export default function Layout({ children }) {
+  return children;
+}
